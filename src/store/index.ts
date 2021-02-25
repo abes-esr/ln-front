89b6242239c8436ad7d8cb9d50faa0d984fbfbe9
charalambou <ecu@abes.fr>
2021-02-25 20:36:58 +0100

    ajout nom etab

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,19 +7,21 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    user: { name: "", token: "", isLoggedIn: false, isAdmin: false },
+    user: { name: "", token: "", nameEtab: "", isLoggedIn: false, isAdmin: false },
     darkTheme: false
   },
   mutations: {
     SET_TOKEN(state, token) {
       state.user.token = token.accessToken;
       state.user.name = token.userName;
+      state.user.nameEtab = token.userNameEtab;
       state.user.isLoggedIn = true;
       state.user.isAdmin = token.isAdmin;
     },
     SET_LOGOUT(state) {
       state.user.token = "";
       state.user.name = "";
+      state.user.nameEtab = "";
       state.user.isLoggedIn = false;
       state.user.isAdmin = false;
     },
@@ -30,7 +32,7 @@ export default new Vuex.Store({
   actions: {
     login({ commit }, credentials) {
       return axios
-        .post(process.env.VUE_APP_ROOT_API + "login", {
+        .post("http://localhost:8080/" + "login", {
           login: credentials.siren,
           password: credentials.password
         })
@@ -48,6 +50,9 @@ export default new Vuex.Store({
   getters: {
     userName: state => {
       return state.user.name;
+    },
+    userEtab: state => {
+      return state.user.nameEtab;
     },
     token: state => {
       return state.user.token;
