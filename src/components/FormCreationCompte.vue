797f2b933a60d33e4e919ceedcb4a8b58534abcd
charalambou <ecu@abes.fr>
2021-03-18 09:50:27 +0100

    oeil sur input password + nettoyage index.html + supp Helloworld Home

--- src/components/FormCreationCompte.vue
+++ src/components/FormCreationCompte.vue
@@ -2,256 +2,262 @@
   <v-container fill-height class="d-flex justify-center">
     <v-row align="center" justify="center">
       <v-col lg="5" md="8" xs="10">
-  <div>
-    <v-card witdh="100%">
-      <v-form ref="formCreationCompte" lazy-validation>
-        <v-card-title>Création d'un compte institutionnel</v-card-title>
-        <v-card-text>
-          <v-row>
-            <v-col cols="1" />
-            <v-col cols="10">
-              <v-text-field
-                outlined
-                label="NOM DE L'ETABLISSEMENT"
-                placeholder="NOM DE L'ETABLISSEMENT"
-                v-model="nomEtab"
-                :rules="nomEtabRules"
-                required
-                @keyup.enter="validate()"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="1" />
-            <v-col cols="10">
-              <v-text-field
-                  outlined
-                  label="SIREN"
-                  placeholder="SIREN"
-                  v-model="sirenEtab"
-                  :rules="sirenEtabRules"
-                  required
-                  @keyup.enter="validate()"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="1" />
-            <v-col cols="10">
-              <v-select
-                  outlined
-                  v-model="typeEtab"
-                  :items="typesEtab"
-                  label="Type de l'établissement"
-                  :rules="typeEtabRules"
-                  required
-              ></v-select>
-            </v-col>
-          </v-row>
-          <v-card-title>Informations contact</v-card-title>
-          <v-row>
-            <v-col cols="1" />
-            <v-col cols="10">
-              <v-text-field
-                  outlined
-                  label="Nom"
-                  placeholder="Nom"
-                  v-model="nomContact"
-                  :rules="nomContactRules"
-                  required
-                  @keyup.enter="validate()"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="1" />
-            <v-col cols="10">
-              <v-text-field
-                  outlined
-                  label="Prénom"
-                  placeholder="Prénom"
-                  v-model="prenomContact"
-                  :rules="prenomContactRules"
-                  required
-                  @keyup.enter="validate()"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="1" />
-            <v-col cols="10">
-              <v-text-field
-                  outlined
-                  label="Adresse postale"
-                  placeholder="Adresse postale"
-                  v-model="adresseContact"
-                  :rules="adresseContactRules"
-                  required
-                  @keyup.enter="validate()"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="1" />
-            <v-col cols="10">
-              <v-text-field
-                  outlined
-                  label="Boîte postale"
-                  placeholder="Boîte postale"
-                  v-model="boitePostaleContact"
-                  @keyup.enter="validate()"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="1" />
-            <v-col cols="10">
-              <v-text-field
-                  outlined
-                  label="Code postal"
-                  placeholder="Code postal"
-                  v-model="codePostalContact"
-                  :rules="codePostalContactRules"
-                  required
-                  @keyup.enter="validate()"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="1" />
-            <v-col cols="10">
-              <v-text-field
-                  outlined
-                  label="Ville"
-                  placeholder="Ville"
-                  v-model="villeContact"
-                  :rules="villeContactRules"
-                  required
-                  @keyup.enter="validate()"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="1" />
-            <v-col cols="10">
-              <v-text-field
-                  outlined
-                  label="Cedex"
-                  placeholder="Cedex"
-                  v-model="cedexContact"
-                  @keyup.enter="validate()"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="1" />
-            <v-col cols="10">
-              <v-text-field
-                  outlined
-                  label="Téléphone"
-                  placeholder="Téléphone"
-                  v-model="telContact"
-                  :rules="telContactRules"
-                  required
-                  @keyup.enter="validate()"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="1" />
-            <v-col cols="10">
-              <v-text-field
-                  outlined
-                  label="Adresse e-mail"
-                  placeholder="Adresse e-mail"
-                  v-model="emailContact"
-                  :rules="emailContactRules"
-                  required
-                  @keyup.enter="validate()"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="1" />
-            <v-col cols="10">
-              <v-text-field
-                  outlined
-                  label="Confirmez votre adresse e-mail"
-                  placeholder="Confirmez votre adresse e-mail"
-                  v-model="confirmEmailContact"
-                  :rules="confirmEmailContactRules.concat(confirmEmailContactRule)"
-                  required
-                  @keyup.enter="validate()"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="1" />
-            <v-col cols="10">
-              <v-alert
-                  border="left"
-                  color="grey"
-                  dark
-              >
-                Votre mot de passe doit contenir au minimum 8 caractères dont une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial parmis @ $ ! % * ? &
-              </v-alert>
-              <v-text-field
-                  outlined
-                  label="Mot de passe"
-                  placeholder="Mot de passe"
-                  v-model="passContact"
-                  :rules="passContactRules"
-                  required
-                  @keyup.enter="validate()"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="1" />
-            <v-col cols="10">
-              <v-text-field
-                  outlined
-                  label="Confirmez votre mot de passe"
-                  placeholder="Confirmez votre mot de passe"
-                  v-model="confirmPassContact"
-                  :rules="confirmPassContactRules.concat(confirmPassContactRule)"
-                  required
-                  @keyup.enter="validate()"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="1" />
-            <v-col cols="10">
+        <div>
+          <v-card witdh="100%">
+            <v-form ref="formCreationCompte" lazy-validation>
+              <v-card-title>Création d'un compte institutionnel</v-card-title>
+              <v-card-text>
+                <v-row>
+                  <v-col cols="1" />
+                  <v-col cols="10">
+                    <v-text-field
+                      outlined
+                      label="NOM DE L'ETABLISSEMENT"
+                      placeholder="NOM DE L'ETABLISSEMENT"
+                      v-model="nomEtab"
+                      :rules="nomEtabRules"
+                      required
+                      @keyup.enter="validate()"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="1" />
+                  <v-col cols="10">
+                    <v-text-field
+                        outlined
+                        label="SIREN"
+                        placeholder="SIREN"
+                        v-model="sirenEtab"
+                        :rules="sirenEtabRules"
+                        required
+                        @keyup.enter="validate()"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="1" />
+                  <v-col cols="10">
+                    <v-select
+                        outlined
+                        v-model="typeEtab"
+                        :items="typesEtab"
+                        label="Type de l'établissement"
+                        :rules="typeEtabRules"
+                        required
+                    ></v-select>
+                  </v-col>
+                </v-row>
+                <v-card-title>Informations contact</v-card-title>
+                <v-row>
+                  <v-col cols="1" />
+                  <v-col cols="10">
+                    <v-text-field
+                        outlined
+                        label="Nom"
+                        placeholder="Nom"
+                        v-model="nomContact"
+                        :rules="nomContactRules"
+                        required
+                        @keyup.enter="validate()"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="1" />
+                  <v-col cols="10">
+                    <v-text-field
+                        outlined
+                        label="Prénom"
+                        placeholder="Prénom"
+                        v-model="prenomContact"
+                        :rules="prenomContactRules"
+                        required
+                        @keyup.enter="validate()"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="1" />
+                  <v-col cols="10">
+                    <v-text-field
+                        outlined
+                        label="Adresse postale"
+                        placeholder="Adresse postale"
+                        v-model="adresseContact"
+                        :rules="adresseContactRules"
+                        required
+                        @keyup.enter="validate()"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="1" />
+                  <v-col cols="10">
+                    <v-text-field
+                        outlined
+                        label="Boîte postale"
+                        placeholder="Boîte postale"
+                        v-model="boitePostaleContact"
+                        @keyup.enter="validate()"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="1" />
+                  <v-col cols="10">
+                    <v-text-field
+                        outlined
+                        label="Code postal"
+                        placeholder="Code postal"
+                        v-model="codePostalContact"
+                        :rules="codePostalContactRules"
+                        required
+                        @keyup.enter="validate()"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="1" />
+                  <v-col cols="10">
+                    <v-text-field
+                        outlined
+                        label="Ville"
+                        placeholder="Ville"
+                        v-model="villeContact"
+                        :rules="villeContactRules"
+                        required
+                        @keyup.enter="validate()"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="1" />
+                  <v-col cols="10">
+                    <v-text-field
+                        outlined
+                        label="Cedex"
+                        placeholder="Cedex"
+                        v-model="cedexContact"
+                        @keyup.enter="validate()"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="1" />
+                  <v-col cols="10">
+                    <v-text-field
+                        outlined
+                        label="Téléphone"
+                        placeholder="Téléphone"
+                        v-model="telContact"
+                        :rules="telContactRules"
+                        required
+                        @keyup.enter="validate()"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="1" />
+                  <v-col cols="10">
+                    <v-text-field
+                        outlined
+                        label="Adresse e-mail"
+                        placeholder="Adresse e-mail"
+                        v-model="emailContact"
+                        :rules="emailContactRules"
+                        required
+                        @keyup.enter="validate()"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="1" />
+                  <v-col cols="10">
+                    <v-text-field
+                        outlined
+                        label="Confirmez votre adresse e-mail"
+                        placeholder="Confirmez votre adresse e-mail"
+                        v-model="confirmEmailContact"
+                        :rules="confirmEmailContactRules.concat(confirmEmailContactRule)"
+                        required
+                        @keyup.enter="validate()"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="1" />
+                  <v-col cols="10">
+                    <v-alert
+                        border="left"
+                        color="grey"
+                        dark
+                    >
+                      Votre mot de passe doit contenir au minimum 8 caractères dont une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial parmis @ $ ! % * ? &
+                    </v-alert>
+                    <v-text-field
+                        outlined
+                        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                        label="Mot de passe"
+                        placeholder="Mot de passe"
+                        v-model="passContact"
+                        :rules="passContactRules"
+                        :type="show1 ? 'text' : 'password'"
+                        required
+                        @keyup.enter="validate()"
+                        @click:append="show1 = !show1"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="1" />
+                  <v-col cols="10">
+                    <v-text-field
+                        outlined
+                        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                        label="Confirmez votre mot de passe"
+                        placeholder="Confirmez votre mot de passe"
+                        v-model="confirmPassContact"
+                        :rules="confirmPassContactRules.concat(confirmPassContactRule)"
+                        :type="show1 ? 'text' : 'password'"
+                        required
+                        @keyup.enter="validate()"
+                        @click:append="show1 = !show1"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="1" />
+                  <v-col cols="10">
 
-            </v-col>
-          </v-row>
-        </v-card-text>
-        <v-card-actions>
-          <v-row>
-            <v-col cols="9"></v-col>
-            <v-col cols="2">
-              <v-btn
-                color="success"
-                :loading="buttonLoading"
-                x-large
-                @click="recaptcha()"
-                >Envoyer</v-btn
-              >
-              <v-btn @click="clear">
-                Effacer
-              </v-btn>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+              <v-card-actions>
+                <v-row>
+                  <v-col cols="9"></v-col>
+                  <v-col cols="2">
+                    <v-btn
+                      color="success"
+                      :loading="buttonLoading"
+                      x-large
+                      @click="recaptcha()"
+                      >Envoyer</v-btn
+                    >
+                    <v-btn @click="clear">
+                      Effacer
+                    </v-btn>
 
-            </v-col>
-          </v-row>
-        </v-card-actions>
-      </v-form>
-    </v-card>
-    <br />
-    <v-alert dense outlined :value="alert" type="error">
-      {{ error }}
-    </v-alert>
-  </div>
+                  </v-col>
+                </v-row>
+              </v-card-actions>
+            </v-form>
+          </v-card>
+          <br />
+          <v-alert dense outlined :value="alert" type="error">
+            {{ error }}
+          </v-alert>
+        </div>
       </v-col>
     </v-row>
   </v-container>
@@ -262,7 +268,7 @@
 
 import Vue from "vue";
 import { mapActions } from "vuex";
-import axios from "axios";
+//import axios from "axios";
 
 
 
@@ -270,6 +276,7 @@ export default Vue.extend({
   name: "FormCreationCompte",
   data() {
     return {
+      show1: false,
       token:  this.$recaptchaLoaded() as any,
       nomEtab: "" as string,
       nomEtabRules: [
@@ -349,7 +356,7 @@ export default Vue.extend({
       ],
       confirmPassContact: "" as string,
       confirmPassContactRules: [
-        (v: any) => !!v || "Vous devez confirmer le mot de passe du contact",
+        (v: any) => !!v || "Vous devez confirmer le mot de passe du contact"
       ],
       idAbes: "" as string,
       buttonLoading: false,
@@ -446,10 +453,10 @@ export default Vue.extend({
     },
 
     clear() {
-      this.$refs.formCreationCompte.reset();
-    },
+      //this.$refs.formCreationCompte.reset();
+    }
   }
-})
+});
 
 </script>
 
