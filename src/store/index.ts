c4408ecadbd9e2f4a6cbf03681ca6bc471ed4853
charalambou <ecu@abes.fr>
2021-03-22 15:28:39 +0100

    clarification noms attributs user

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,26 +7,20 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    user: {
-      name: "",
-      token: "",
-      nameEtab: "",
-      isLoggedIn: false,
-      isAdmin: false
-    },
+    user: { siren: "", token: "", nameEtab: "", isLoggedIn: false, isAdmin: false },
     darkTheme: false
   },
   mutations: {
     SET_TOKEN(state, token) {
       state.user.token = token.accessToken;
-      state.user.name = token.userName;
-      state.user.nameEtab = token.userNameEtab;
+      state.user.siren = token.siren;
+      state.user.nameEtab = token.nameEtab;
       state.user.isLoggedIn = true;
       state.user.isAdmin = token.isAdmin;
     },
     SET_LOGOUT(state) {
       state.user.token = "";
-      state.user.name = "";
+      state.user.siren = "";
       state.user.nameEtab = "";
       state.user.isLoggedIn = false;
       state.user.isAdmin = false;
@@ -38,24 +32,24 @@ export default new Vuex.Store({
   actions: {
     login({ commit }, credentials) {
       return axios
-        .post(process.env.VUE_APP_ROOT_API + "login", {
-          login: credentials.siren,
-          password: credentials.password
-        })
-        .then(result => {
-          commit("SET_TOKEN", result.data);
-        });
+          .post(process.env.VUE_APP_ROOT_API + "login", {
+            login: credentials.siren,
+            password: credentials.password
+          })
+          .then(result => {
+            commit("SET_TOKEN", result.data);
+          });
     },
     logout({ commit }) {
       commit("SET_LOGOUT");
     },
     changeTheme({ commit }) {
       commit("SET_THEME");
-    }
+    },
   },
   getters: {
-    userName: state => {
-      return state.user.name;
+    userSiren: state => {
+      return state.user.siren;
     },
     userEtab: state => {
       return state.user.nameEtab;
