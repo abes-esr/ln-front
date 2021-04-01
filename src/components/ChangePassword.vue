b5dafa1e193617e071c859176e14afc0be222e9b
Théo Chambon <chambon@abes.fr>
2021-04-01 17:02:08 +0200

    fix errors

--- src/components/ChangePassword.vue
+++ src/components/ChangePassword.vue
@@ -95,7 +95,8 @@ export default Vue.extend({
       this.alert = false;
       this.error = "";
       if ((this.$refs.form as Vue & { validate: () => boolean }).validate())
-       // this.sumbit();
+        // this.sumbit();
+        console.log("");
     },
     submit(): void {
       this.buttonLoading = true;
