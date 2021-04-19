fac615f952c17253b0c233b4c13e6deebaf49b22
SimonLmrr <70648447+SimonLmrr@users.noreply.github.com>
2021-04-19 15:52:02 +0200

    Formulaire de fusion

--- src/components/FormEtab.vue
+++ src/components/FormEtab.vue
@@ -116,11 +116,10 @@
               required
               @keyup.enter="validate()"
           ></v-text-field>
-          <v-alert
-              border="left"
-              color="grey"
-              dark>
-            Votre mot de passe doit contenir au minimum 8 caractères dont une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial parmis @ $ ! % * ? &
+          <v-alert border="left" color="grey" dark>
+            Votre mot de passe doit contenir au minimum 8 caractères dont une
+            lettre majuscule, une lettre minuscule, un chiffre et un caractère
+            spécial parmis @ $ ! % * ? &
           </v-alert>
           <v-text-field
               outlined
@@ -147,7 +146,7 @@
 
 <script lang="ts">
 import Vue from "vue";
-import {mapActions} from "vuex";
+import { mapActions } from "vuex";
 
 export default Vue.extend({
   name: "FormEtab",
@@ -156,7 +155,10 @@ export default Vue.extend({
       nomEtab: "" as string,
       nomEtabRules: [
         (v: any) => !!v || "Le nom de l'établissement est obligatoire",
-        (v: any) => /^([0-9A-Za-z'àâéèêôùûçÀÂÉÈÔÙÛÇ,\s-]{5,80})$/.test(v) || "Le nom d'établissement fourni n'est pas valide"],
+        (v: any) =>
+            /^([0-9A-Za-z'àâéèêôùûçÀÂÉÈÔÙÛÇ,\s-]{5,80})$/.test(v) ||
+            "Le nom d'établissement fourni n'est pas valide"
+      ],
 
       sirenEtab: "" as string,
       sirenEtabRules: [
@@ -164,7 +166,8 @@ export default Vue.extend({
         (v: any) => /^\d{9}$/.test(v) || "Le SIREN doit contenir 9 chiffres"
       ],
 
-      typesEtab: ["EPIC/EPST",
+      typesEtab: [
+        "EPIC/EPST",
         "Ecoles d'ingénieurs",
         "Ecoles de formation spécialisée",
         "Ecoles de Management",
@@ -176,77 +179,115 @@ export default Vue.extend({
         "Lecture publique",
         "Universités",
         "Etablissement membre du réseau Latitude France",
-        "Autre"],
+        "Autre"
+      ],
       typeEtab: "" as string,
       typeEtabRules: [
-        (v: any) => !!v || "Le type de l'établissement est obligatoire",
+        (v: any) => !!v || "Le type de l'établissement est obligatoire"
       ],
 
       nomContact: "" as string,
       nomContactRules: [
         (v: any) => !!v || "Le nom du contact est obligatoire",
-        (v: any) => /^([A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+(( |')[A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+)*)+([-]([A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+(( |')[A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+)*)+)*$/.test(v) || "Le nom fourni n'est pas valide"
+        (v: any) =>
+            /^([A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+(( |')[A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+)*)+([-]([A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+(( |')[A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+)*)+)*$/.test(
+                v
+            ) || "Le nom fourni n'est pas valide"
       ],
       prenomContact: "" as string,
       prenomContactRules: [
         (v: any) => !!v || "Le prénom du contact est obligatoire",
-        (v: any) => /^([A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+(( |')[A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+)*)+([-]([A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+(( |')[A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+)*)+)*$/.test(v) || "Le prénom fourni n'est pas valide"
+        (v: any) =>
+            /^([A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+(( |')[A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+)*)+([-]([A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+(( |')[A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+)*)+)*$/.test(
+                v
+            ) || "Le prénom fourni n'est pas valide"
       ],
       adresseContact: "" as string,
       adresseContactRules: [
-        (v: any) => !!v || "L'adresse postale de l'établissement est obligatoire",
-        (v: any) => /^([0-9A-Za-z'àâéèêôùûçÀÂÉÈÔÙÛÇ,\s-]{5,80})$/.test(v) || "L'adresse postale fournie n'est pas valide"
+        (v: any) =>
+            !!v || "L'adresse postale de l'établissement est obligatoire",
+        (v: any) =>
+            /^([0-9A-Za-z'àâéèêôùûçÀÂÉÈÔÙÛÇ,\s-]{5,80})$/.test(v) ||
+            "L'adresse postale fournie n'est pas valide"
       ],
       boitePostaleContact: "" as string,
       codePostalContact: "" as string,
       codePostalContactRules: [
         (v: any) => !!v || "Le code postal de l'établissement est obligatoire",
-        (v: any) => /^\d{5}$/.test(v) || "Le code postal fourni n'est pas valide"
+        (v: any) =>
+            /^\d{5}$/.test(v) || "Le code postal fourni n'est pas valide"
       ],
       villeContact: "" as string,
       villeContactRules: [
         (v: any) => !!v || "La ville de l'établissement est obligatoire",
-        (v: any) => /^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$/.test(v) || "La ville fournie n'est pas valide"
+        (v: any) =>
+            /^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$/.test(
+                v
+            ) || "La ville fournie n'est pas valide"
       ],
       cedexContact: "" as string,
       telContact: "" as string,
       telContactRules: [
         (v: any) => !!v || "Le téléphone du contact est obligatoire",
-        (v: any) => /^\d{10}$/.test(v) || "Veuillez entrer 10 chiffres sans espace"
+        (v: any) =>
+            /^\d{10}$/.test(v) || "Veuillez entrer 10 chiffres sans espace"
       ],
       emailContact: "" as string,
       emailContactRules: [
         (v: any) => !!v || "L'adresse mail du contact est obligatoire",
-        (v: any) => /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(v) || "L'adresse mail fournie n'est pas valide"
+        (v: any) =>
+            /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(v) ||
+            "L'adresse mail fournie n'est pas valide"
       ],
       confirmEmailContact: "" as string,
       confirmEmailContactRules: [
-        (v: any) => !!v || "Vous devez confirmer l'adresse mail du contact",
+        (v: any) => !!v || "Vous devez confirmer l'adresse mail du contact"
       ],
       passContact: "" as string,
       passContactRules: [
         (v: any) => !!v || "Le mot de passe du contact est obligatoire",
-        (v: any) => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(v) || "Le mot de passe fourni n'est pas valide"
+        (v: any) =>
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+                v
+            ) || "Le mot de passe fourni n'est pas valide"
       ],
       confirmPassContact: "" as string,
       confirmPassContactRules: [
-        (v: any) => !!v || "Vous devez confirmer le mot de passe du contact",
+        (v: any) => !!v || "Vous devez confirmer le mot de passe du contact"
       ],
       idAbes: "" as string
     };
   },
-  props: ['bus'],
-  methods:{
+  props: ["bus"],
+  methods: {
     validAndSend(): void {
-      console.log("Validation")
-      this.$emit("formEtab", {"nomEtab":this.nomEtab} )
-/*      if ((this.$refs.form as Vue & { validate: () => boolean }).validate()) {
+      console.log("Validation");
+      if ((this.$refs.form as Vue & { validate: () => boolean }).validate()) {
+        this.$emit("formEtab", {
+          adresseContact: this.adresseContact,
+          boitePostaleContact: this.boitePostaleContact,
+          cedexContact: this.cedexContact,
+          codePostalContact: this.codePostalContact,
+          idAbes: this.idAbes,
+          mailContact: this.emailContact,
+          motDePasse: this.passContact,
+          nom: this.nomEtab,
+          nomContact: this.nomContact,
+          prenomContact: this.prenomContact,
+          siren: this.sirenEtab,
+          telephoneContact: this.telContact,
+          typeEtablissement: this.typeEtab,
+          villeContact: this.villeContact
+        });
+      }
+
+      /*      if ((this.$refs.form as Vue & { validate: () => boolean }).validate()) {
         this.$emit("formEtab", {"nomEtab":this.nomEtab} )
       }*/
     }
   },
   mounted() {
-    this.bus.$on('submit', this.validAndSend)
+    this.bus.$on("submit", this.validAndSend);
   }
 });
 </script>
--- src/components/FormFusionEtablissement.vue
+++ src/components/FormFusionEtablissement.vue
@@ -1,39 +1,111 @@
 <template>
   <div>
-    <p>Fusion</p>
-    <form-etab
-        v-for="n in formNumber"
-        :key="n"
-        :bus="bus"
-        v-on:formEtab="send"></form-etab>
-    <v-btn @click="triggerChildremForm()">Valider</v-btn>
+    <v-card width="100%">
+      <v-form ref="form" lazy-validation>
+        <v-card-text>
+          <v-row>
+            <v-col lg="12" md="12" xs="12">
+              <v-row>
+                <v-col cols="2" />
+                <v-col cols="8">
+                  <v-card-title>Fusion d'établissements</v-card-title>
+                  <v-card width="100%">
+                    <v-card-title
+                    >Siren des établissements à fusionner</v-card-title
+                    >
+                    <v-card-text>
+                      <v-text-field
+                          outlined
+                          label="SIREN"
+                          placeholder="SIREN"
+                          v-model="sirenEtab[n - 1]"
+                          :rules="sirenEtabRules"
+                          required
+                          @keyup.enter="validate()"
+                          v-for="n in sirenNumber"
+                          :key="n"
+                      ></v-text-field>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-btn @click="increaseSirenNumber"
+                      >Ajouter un siren</v-btn
+                      >
+                      <v-btn @click="decreaseSirenNumber"
+                      >Supprimer un siren</v-btn
+                      >
+                    </v-card-actions>
+                  </v-card>
+                  <form-etab :bus="bus" v-on:formEtab="send"></form-etab>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <v-row>
+            <v-col cols="9"></v-col>
+            <v-col>
+              <v-btn @click="triggerChildremForm()" color="success"
+              >Valider
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card-actions>
+      </v-form>
+    </v-card>
   </div>
 </template>
 
 <script lang="ts">
 import FormEtab from "@/components/FormEtab.vue";
 import Vue from "vue";
+import { HTTP } from "../utils/http-commons";
 
 export default Vue.extend({
   name: "FormFusionEtablissement",
-  components: {FormEtab},
+  components: { FormEtab },
   data() {
     return {
-      formNumber: 2,
-      bus: new Vue()
+      sirenEtab: [],
+      sirenEtabRules: [
+        (v: any) => !!v || "SIREN obligatoire",
+        (v: any) => /^\d{9}$/.test(v) || "Le SIREN doit contenir 9 chiffres"
+      ],
+      bus: new Vue(),
+      sirenNumber: 2
     };
   },
   methods: {
     triggerChildremForm(): void {
-      this.bus.$emit('submit');
+      this.bus.$emit("submit");
     },
     send(payload: object): void {
-      console.log(payload);
+      if ((this.$refs.form as Vue & { validate: () => boolean }).validate()) {
+        console.log({
+          etablissementDTO: payload,
+          sirenFusionnes: this.sirenEtab
+        });
+        HTTP.post(process.env.VUE_APP_ROOT_API + "/ln/", {
+          etablissementDTO: payload,
+          sirenFusionnes: this.sirenEtab
+        })
+            .then(res => {
+              res.status;
+            })
+            .catch(err => {
+              err.data();
+            });
+      }
+    },
+    increaseSirenNumber: function() {
+      this.sirenNumber++;
+    },
+    decreaseSirenNumber: function() {
+      this.sirenNumber--;
+      this.sirenEtab.pop();
     }
   }
-})
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
