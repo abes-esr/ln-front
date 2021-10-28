c83472bd7757236707ddfa13530d6de48a5bb3b7
SamuelQuetin <samquetinuel@gmail.com>
2021-10-28 14:56:35 +0200

    Tentative forma formulaire creation Etab

--- src/components/etablissement/Contact.vue
+++ src/components/etablissement/Contact.vue
@@ -1,18 +1,8 @@
 <template>
   <v-form ref="form" lazy-validation>
-    <v-col cols="12" md="6" lg="6" xl="6">
-      <v-text-field
-          outlined
-          label="Mail de contact"
-          placeholder="Mail de contact"
-          v-model="contact.mail"
-          :rules="rulesForms.emailContactRules"
-          required
-          @keyup.enter="validate()"
-      ></v-text-field>
-    </v-col>
-    <v-col cols="12" md="6" lg="6" xl="6">
-      <v-text-field
+    <v-row>
+      <v-col cols="12" md="6" lg="6" xl="6">
+        <v-text-field
           outlined
           label="Nom"
           placeholder="Nom"
@@ -20,21 +10,10 @@
           :rules="rulesForms.nomContactRules"
           required
           @keyup.enter="validate()"
-      ></v-text-field>
-    </v-col>
-    <v-col cols="12" md="6" lg="6" xl="6">
-      <v-text-field
-          outlined
-          label="Prénom"
-          placeholder="Prénom"
-          v-model="contact.prenom"
-          :rules="rulesForms.prenomContactRules"
-          required
-          @keyup.enter="validate()"
-      ></v-text-field>
-    </v-col>
-    <v-col cols="12" md="6" lg="6" xl="6">
-      <v-text-field
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" md="6" lg="6" xl="6">
+        <v-text-field
           outlined
           label="Téléphone"
           placeholder="Téléphone"
@@ -43,10 +22,36 @@
           :rules="rulesForms.telContactRules"
           required
           @keyup.enter="validate()"
-      ></v-text-field>
-    </v-col>
-    <v-col cols="12" md="6" lg="6" xl="6">
-      <v-text-field
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="6" lg="6" xl="6">
+        <v-text-field
+          outlined
+          label="Prénom"
+          placeholder="Prénom"
+          v-model="contact.prenom"
+          :rules="rulesForms.prenomContactRules"
+          required
+          @keyup.enter="validate()"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" md="6" lg="6" xl="6">
+        <v-text-field
+          outlined
+          label="Mail de contact"
+          placeholder="Mail de contact"
+          v-model="contact.mail"
+          :rules="rulesForms.emailContactRules"
+          required
+          @keyup.enter="validate()"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="6" lg="6" xl="6">
+        <v-text-field
           outlined
           label="Adresse"
           placeholder="Adresse"
@@ -55,20 +60,42 @@
           :rules="rulesForms.adresseContactRules"
           required
           @keyup.enter="validate()"
-      ></v-text-field>
-    </v-col>
-    <v-col cols="12" md="6" lg="6" xl="6">
-      <v-text-field
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" md="6" lg="6" xl="6">
+        <v-text-field
+          outlined
+          label="Confirmez votre adresse e-mail"
+          placeholder="Confirmez votre adresse e-mail"
+          v-model="emailConfirmation"
+          :rules="rulesForms.confirmEmailContactRules"
+          required
+          @keyup.enter="validate()"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="6" lg="6" xl="6">
+        <v-text-field
           outlined
           label="Boite Postal"
           placeholder="Boite Postal"
           v-model="contact.boitePostale"
           required
           @keyup.enter="validate()"
-      ></v-text-field>
-    </v-col>
-    <v-col cols="12" md="6" lg="6" xl="6">
-      <v-text-field
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" md="6" lg="6" xl="6">
+        <v-alert border="left" type="info" outlined>
+          Votre mot de passe doit contenir au minimum 8 caractères dont une
+          lettre majuscule, une lettre minuscule, un chiffre et un caractère
+          spécial parmis @ $ ! % * ? &
+        </v-alert>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="6" lg="6" xl="6">
+        <v-text-field
           outlined
           label="Code Postal"
           placeholder="Code Postal"
@@ -77,51 +104,10 @@
           :rules="rulesForms.codePostalContactRules"
           required
           @keyup.enter="validate()"
-      ></v-text-field>
-    </v-col>
-    <v-col cols="12" md="6" lg="6" xl="6">
-      <v-text-field
-          outlined
-          label="Ville"
-          placeholder="Ville"
-          v-model="contact.ville"
-          :rules="rulesForms.villeContactRules"
-          required
-          @keyup.enter="validate()"
-      ></v-text-field>
-    </v-col>
-    <v-col cols="12" md="6" lg="6" xl="6">
-      <v-text-field
-          outlined
-          label="CEDEX"
-          placeholder="CEDEX"
-          v-model="contact.cedex"
-          required
-          @keyup.enter="validate()"
-      ></v-text-field>
-    </v-col>
-
-    <v-col cols="12" md="6" lg="6" xl="6">
-      <v-text-field
-          outlined
-          label="Confirmez votre adresse e-mail"
-          placeholder="Confirmez votre adresse e-mail"
-          v-model="emailConfirmation"
-          :rules="
-                   rulesForms.confirmEmailContactRules
-                  "
-          required
-          @keyup.enter="validate()"
-      ></v-text-field>
-    </v-col>
-    <v-col cols="12" md="6" lg="6" xl="6">
-
-      <v-alert border="left" type="info" outlined>
-        Votre mot de passe doit contenir au minimum 8 caractères dont
-        une lettre majuscule, une lettre minuscule, un chiffre et un
-        caractère spécial parmis @ $ ! % * ? &
-      </v-alert>
-      <v-text-field
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" md="6" lg="6" xl="6">
+        <v-text-field
           outlined
           :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
           label="Mot de passe"
@@ -132,31 +118,55 @@
           required
           @keyup.enter="validate()"
           @click:append="show1 = !show1"
-      ></v-text-field>
-    </v-col>
-    <v-col cols="12" md="6" lg="6" xl="6">
-      <v-text-field
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="6" lg="6" xl="6">
+        <v-text-field
+          outlined
+          label="Ville"
+          placeholder="Ville"
+          v-model="contact.ville"
+          :rules="rulesForms.villeContactRules"
+          required
+          @keyup.enter="validate()"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" md="6" lg="6" xl="6">
+        <v-text-field
           outlined
           :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
           label="Confirmez votre mot de passe"
           placeholder="Confirmez votre mot de passe"
           v-model="motDePassConfirmation"
-          :rules="
-                    rulesForms.confirmPassContactRules
-                  "
+          :rules="rulesForms.confirmPassContactRules"
           :type="show1 ? 'text' : 'password'"
           required
           @keyup.enter="validate()"
           @click:append="show1 = !show1"
-      ></v-text-field>
-    </v-col>
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="6" lg="6" xl="6">
+        <v-text-field
+          outlined
+          label="CEDEX"
+          placeholder="CEDEX"
+          v-model="contact.cedex"
+          required
+          @keyup.enter="validate()"
+        ></v-text-field>
+      </v-col>
+    </v-row>
   </v-form>
 </template>
 
 <script lang="ts">
-import {Component, Prop, Vue} from "vue-property-decorator";
-import {rulesForms} from "@/core/RulesForm";
-import {Action} from "@/core/CommonDefinition";
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { rulesForms } from "@/core/RulesForm";
+import { Action } from "@/core/CommonDefinition";
 import ContactEtablissement from "@/core/ContactEtablissement";
 
 @Component
--- src/components/etablissement/FormEtablissement.vue
+++ src/components/etablissement/FormEtablissement.vue
@@ -1,12 +1,12 @@
 <template>
   <v-card>
     <v-form ref="formCreationCompte" lazy-validation>
-      <v-card-title v-if="action === Action.CREATION" class="pl-3">
-        Créer le compte de votre établissement
-      </v-card-title>
       <v-card-subtitle @click="$router.push({ path: '/login' })">
         Votre établissement a déjà un compte ? S'authentifier
       </v-card-subtitle>
+      <v-card-title v-if="action === Action.CREATION" class="pl-3">
+        Créer le compte de votre établissement
+      </v-card-title>
       <v-card-text>
         <v-row v-if="action === Action.CREATION">
           <v-col cols="12" md="6" lg="6" xl="6">
@@ -23,122 +23,111 @@
                 à la disposition des professionnels de la documentation cette
                 application dédiée à la gestion des accès.
                 <a
-                    href="http://documentation.abes.fr/aidelicencesnationales/index.html#Beneficiaires"
-                    target="_blank"
-                >Vérifier l'éligibilité de votre établissement.</a
+                  href="http://documentation.abes.fr/aidelicencesnationales/index.html#Beneficiaires"
+                  target="_blank"
+                  >Vérifier l'éligibilité de votre établissement.</a
                 >
               </p>
               <v-checkbox
-                  required
-                  :rules="rulesForms.checkboxRules"
-                  label="Je confirme que mon établissement est éligible"
+                required
+                :rules="rulesForms.checkboxRules"
+                label="Je confirme que mon établissement est éligible"
               ></v-checkbox>
             </v-alert>
           </v-col>
         </v-row>
         <v-row
-            v-if="
+          v-if="
             (action === Action.MODIFICATION && isAdmin) ||
               action === Action.CREATION
           "
         >
           <v-card-title>Informations de l'établissement</v-card-title>
+        </v-row>
+        <v-row>
           <v-divider></v-divider>
-          <v-row>
-            <v-col cols="12" md="6" lg="6" xl="6">
-              <v-text-field
-                  outlined
-                  label="Nom de l'établissement"
-                  placeholder="Nom de l'établissement"
-                  maxlength="80"
-                  v-model="etablissement.nom"
-                  :rules="rulesForms.nomEtabRules"
-                  required
-                  @keyup.enter="validate()"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="6" lg="6" xl="6">
-              <v-text-field
-                  outlined
-                  label="SIREN"
-                  placeholder="SIREN"
-                  maxlength="9"
-                  v-model="etablissement.siren"
-                  :rules="rulesForms.sirenEtabRules"
-                  required
-                  @input="checkSiren()"
-                  @keyup.enter="validate()"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="6" lg="6" xl="6">
-              <!-- POPUP CONFIRMATION SIREN -->
-              <v-dialog v-model="dialog" persistent max-width="450">
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn
-                      :disabled="!dialogAvailable"
-                      rounded
-                      outlined
-                      v-bind="attrs"
-                      v-on="on"
-                      color="primary"
-                  >Valider
-                  </v-btn>
-                </template>
-                <v-card>
-                  <v-card-title class="headline">
-                    Votre SIREN est-il correct ?
-                  </v-card-title>
-                  <v-card-text
-                  >Le SIREN renseigné correspond à l'établissement suivant
-                    : <b>{{ checkSirenAPI }}</b></v-card-text
-                  >
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn
-                        color="green darken-1"
-                        text
-                        @click="dialog = false"
-                    >
-                      Non
-                    </v-btn>
-                    <v-btn
-                        color="green darken-1"
-                        text
-                        @click="dialog = false"
-                    >
-                      Oui
-                    </v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-              <!-- FIN POPUP CONFIRMATION SIREN -->
-            </v-col>
-          </v-row>
-          <v-row class="mt-0">
-            <v-col cols="12" md="6" lg="6" xl="6">
-              <v-chip
-                  class="ma-2"
-                  :color="checkSirenColor"
-                  text-color="white"
-              >SIREN : {{ checkSirenAPI }}
-              </v-chip>
-            </v-col>
-          </v-row>
+        </v-row>
+        <v-row class="pt-1">
+          <v-col cols="12" md="6" lg="6" xl="6">
+            <v-text-field
+              outlined
+              label="Nom de l'établissement"
+              placeholder="Nom de l'établissement"
+              maxlength="80"
+              v-model="etablissement.nom"
+              :rules="rulesForms.nomEtabRules"
+              required
+              @keyup.enter="validate()"
+            ></v-text-field>
+          </v-col>
           <v-col cols="12" md="6" lg="6" xl="6">
             <v-select
-                outlined
-                v-model="etablissement.typeEtablissement"
-                :items="typesEtab"
-                label="Type de l'établissement"
-                :rules="rulesForms.typeEtabRules"
-                required
+              outlined
+              v-model="etablissement.typeEtablissement"
+              :items="typesEtab"
+              label="Type de l'établissement"
+              :rules="rulesForms.typeEtabRules"
+              required
             ></v-select>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="6" lg="6" xl="6">
+            <v-text-field
+              outlined
+              label="SIREN"
+              placeholder="SIREN"
+              maxlength="9"
+              v-model="etablissement.siren"
+              :rules="rulesForms.sirenEtabRules"
+              required
+              @input="checkSiren()"
+              @keyup.enter="validate()"
+            ></v-text-field>
+            <v-chip class="ma-2" :color="checkSirenColor" text-color="white"
+              >SIREN : {{ checkSirenAPI }}
+            </v-chip>
+            <!-- POPUP CONFIRMATION SIREN -->
+            <v-dialog v-model="dialog" persistent max-width="450">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  :disabled="!dialogAvailable"
+                  rounded
+                  outlined
+                  v-bind="attrs"
+                  v-on="on"
+                  color="primary"
+                  >Valider
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title class="headline">
+                  Votre SIREN est-il correct ?
+                </v-card-title>
+                <v-card-text
+                  >Le SIREN renseigné correspond à l'établissement suivant :
+                  <b>{{ checkSirenAPI }}</b></v-card-text
+                >
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="green darken-1" text @click="dialog = false">
+                    Non
+                  </v-btn>
+                  <v-btn color="green darken-1" text @click="dialog = false">
+                    Oui
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+            <!-- FIN POPUP CONFIRMATION SIREN -->
+          </v-col>
+          <v-col cols="12" md="6" lg="6" xl="6">
             <v-btn
-                color="primary"
-                rounded
-                href="https://www.sirene.fr/sirene/public/static/recherche"
-                target="_blank"
-                outlined
+              color="primary"
+              rounded
+              href="https://www.sirene.fr/sirene/public/static/recherche"
+              target="_blank"
+              outlined
             >
               Trouver le SIREN de votre établissement ?
             </v-btn>
@@ -148,20 +137,26 @@
           <v-card outlined>
             <v-card-title>Information Contact</v-card-title>
             <v-card-text>
-              <contact :action="action" :contact="etablissement.contact"/>
-              <v-col cols="12" md="6" lg="6" xl="6" v-if="action === Action.CREATION">
+              <contact :action="action" :contact="etablissement.contact" />
+              <v-col
+                cols="12"
+                md="6"
+                lg="6"
+                xl="6"
+                v-if="action === Action.CREATION"
+              >
                 <v-checkbox
-                    required
-                    :rules="rulesForms.checkboxRules"
-                    label="J'accepte les conditions générales liées à la politique de
+                  required
+                  :rules="rulesForms.checkboxRules"
+                  label="J'accepte les conditions générales liées à la politique de
                 confidentialité*"
                 ></v-checkbox>
                 <div class="subtitle-2">
                   Pour connaître et exercer vos droits relatifs à l'utilisation
-                  des données collectées par ce formulaire, veuillez consulter la
-                  page
+                  des données collectées par ce formulaire, veuillez consulter
+                  la page
                   <a @click="$router.push({ path: '/donneespersonnelles' })"
-                  >Données personnelles</a
+                    >Données personnelles</a
                   >
                 </div>
               </v-col>
@@ -177,21 +172,19 @@
             </v-col>
             <v-col cols="3">
               <v-btn
-                  color="button"
-                  :loading="buttonLoading"
-                  x-large
-                  @click="recaptcha()"
-              >Enregistrer
-                <v-icon class="pl-1"
-                >mdi-arrow-right-circle-outline
-                </v-icon>
+                color="button"
+                :loading="buttonLoading"
+                x-large
+                @click="recaptcha()"
+                >Enregistrer
+                <v-icon class="pl-1">mdi-arrow-right-circle-outline</v-icon>
               </v-btn>
             </v-col>
           </v-row>
         </v-col>
       </v-card-actions>
     </v-form>
-    <br/>
+    <br />
     <v-alert dense outlined :value="alert" type="error">
       {{ error }}
     </v-alert>
@@ -199,20 +192,20 @@
 </template>
 
 <script lang="ts">
-import {Component, Prop, Vue} from "vue-property-decorator";
-import {Logger} from "@/utils/Logger";
-import {serviceGouv} from "@/core/service/data.gouv/DataGouvApiService";
-import {SirenNotFoundError} from "@/core/service/data.gouv/exception/SirenNotFoundError";
-import {LicencesNationalesUnauthorizedApiError} from "@/core/service/licencesnationales/exception/LicencesNationalesUnauthorizedApiError";
-import {DataGouvApiError} from "@/core/service/data.gouv/exception/DataGouvApiError";
-import {etablissementService} from "@/core/service/licencesnationales/EtablissementService";
-import {rulesForms} from "@/core/RulesForm";
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Logger } from "@/utils/Logger";
+import { serviceGouv } from "@/core/service/data.gouv/DataGouvApiService";
+import { SirenNotFoundError } from "@/core/service/data.gouv/exception/SirenNotFoundError";
+import { LicencesNationalesUnauthorizedApiError } from "@/core/service/licencesnationales/exception/LicencesNationalesUnauthorizedApiError";
+import { DataGouvApiError } from "@/core/service/data.gouv/exception/DataGouvApiError";
+import { etablissementService } from "@/core/service/licencesnationales/EtablissementService";
+import { rulesForms } from "@/core/RulesForm";
 import Etablissement from "@/core/Etablissement";
-import {Action} from "@/core/CommonDefinition";
+import { Action } from "@/core/CommonDefinition";
 import Contact from "@/components/etablissement/Contact.vue";
 
 @Component({
-  components: {Contact}
+  components: { Contact }
 })
 export default class FormEtablissement extends Vue {
   etablissement: Etablissement;
@@ -261,9 +254,9 @@ export default class FormEtablissement extends Vue {
     //if (this.isHuman(this.recaptcha()) {
     if (this.token != null) {
       if (
-          (this.$refs.formCreationCompte as Vue & {
-            validate: () => boolean;
-          }).validate()
+        (this.$refs.formCreationCompte as Vue & {
+          validate: () => boolean;
+        }).validate()
       ) {
         this.creationCompte();
       }
@@ -275,40 +268,40 @@ export default class FormEtablissement extends Vue {
     this.alert = false;
 
     etablissementService
-        .creerEtablissement(this.etablissement, this.token)
-        .then(response => {
-          this.buttonLoading = false;
-          Logger.debug("notification = " + "l'établissement à bien été créé");
-          //this.setNotification(response.data);
-          this.$store
-              .dispatch("setNotification", "l'établissement à bien été créé")
-              .catch(err => {
-                Logger.error(err);
-              });
-          this.$store.dispatch("setCreationCompteEffectueeTrue").catch(err => {
+      .creerEtablissement(this.etablissement, this.token)
+      .then(response => {
+        this.buttonLoading = false;
+        Logger.debug("notification = " + "l'établissement à bien été créé");
+        //this.setNotification(response.data);
+        this.$store
+          .dispatch("setNotification", "l'établissement à bien été créé")
+          .catch(err => {
             Logger.error(err);
           });
-          Logger.debug("notification = " + this.$store.getters.notification());
-          Logger.debug(
-              "creationCompteEffectuee = " +
-              this.$store.getters.creationCompteEffectuee()
-          );
-          this.$router.push({path: "/login"});
-        })
-        .catch(err => {
-          this.buttonLoading = false;
-          this.alert = true;
-
-          if (err instanceof LicencesNationalesUnauthorizedApiError) {
-            this.error =
-                "Vous n'êtes pas autorisé à effectuer cette opération.: " +
-                err.message;
-            Logger.error(err.toString());
-          } else {
-            Logger.error(err.toString());
-            this.error = "Impossible d'exécuter l'action : " + err.message;
-          }
+        this.$store.dispatch("setCreationCompteEffectueeTrue").catch(err => {
+          Logger.error(err);
         });
+        Logger.debug("notification = " + this.$store.getters.notification());
+        Logger.debug(
+          "creationCompteEffectuee = " +
+            this.$store.getters.creationCompteEffectuee()
+        );
+        this.$router.push({ path: "/login" });
+      })
+      .catch(err => {
+        this.buttonLoading = false;
+        this.alert = true;
+
+        if (err instanceof LicencesNationalesUnauthorizedApiError) {
+          this.error =
+            "Vous n'êtes pas autorisé à effectuer cette opération.: " +
+            err.message;
+          Logger.error(err.toString());
+        } else {
+          Logger.error(err.toString());
+          this.error = "Impossible d'exécuter l'action : " + err.message;
+        }
+      });
   }
 
   /**
@@ -320,25 +313,25 @@ export default class FormEtablissement extends Vue {
     if (this.etablissement.siren.length === 9) {
       this.dialogAvailable = true;
       serviceGouv
-          .checkSiren(this.etablissement.siren)
-          .then(() => {
-            this.checkSirenAPI = "valide";
-            this.checkSirenColor = "green";
-          })
-          .catch(err => {
-            Logger.error(err);
-            if (err instanceof SirenNotFoundError) {
-              this.checkSirenAPI = "inconnu";
-              this.checkSirenColor = "red";
-            } else if (err instanceof DataGouvApiError) {
-              this.checkSirenAPI =
-                  "Impossible de contacter le service de vérification du numéro SIREN";
-              this.checkSirenColor = "red";
-            } else {
-              this.checkSirenAPI = "Erreur interne : " + err.message;
-              this.checkSirenColor = "red";
-            }
-          });
+        .checkSiren(this.etablissement.siren)
+        .then(() => {
+          this.checkSirenAPI = "valide";
+          this.checkSirenColor = "green";
+        })
+        .catch(err => {
+          Logger.error(err);
+          if (err instanceof SirenNotFoundError) {
+            this.checkSirenAPI = "inconnu";
+            this.checkSirenColor = "red";
+          } else if (err instanceof DataGouvApiError) {
+            this.checkSirenAPI =
+              "Impossible de contacter le service de vérification du numéro SIREN";
+            this.checkSirenColor = "red";
+          } else {
+            this.checkSirenAPI = "Erreur interne : " + err.message;
+            this.checkSirenColor = "red";
+          }
+        });
     } else {
       this.dialogAvailable = false;
     }
