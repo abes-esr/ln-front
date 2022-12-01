03ad7f829c8c29ca5ea0f9bffb8d90351d80147a
Théo Chambon <chambon@abes.fr>
2022-12-01 11:51:20 +0100

    Fix lien doc

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,58 +1,24 @@
 <template>
-  <v-app-bar
-    color="primary"
-    flat
-    id="appBar"
-    src="@/assets/img/header/graphe-couleur-appli-ln.png"
-    max-height="150px"
-  >
+  <v-app-bar color="primary" flat id="appBar" src="@/assets/img/header/graphe-couleur-appli-ln.png" max-height="150px">
     <template v-slot:img="{ props }">
-      <v-img
-        v-bind="props"
-        class="hidden-sm-and-down"
-        style="float: right; height: 300%; min-width: 60%; max-width:75%; margin: -180px 0px 0px 0px;"
-      ></v-img>
+      <v-img v-bind="props" class="hidden-sm-and-down"
+        style="float: right; height: 300%; min-width: 60%; max-width:75%; margin: -180px 0px 0px 0px;"></v-img>
     </template>
     <v-layout class="d-flex flex-column" fill-height>
-      <v-container
-        class="justify-start pa-0"
-        style="margin-left: 2%; height: 100%"
-      >
-        <v-toolbar-title
-          id="titleBar"
-          v-on:click="$router.push({ name: 'Home' }).catch(err => {})"
-        >
-          <v-img
-            alt="Logo licences nationales"
-            src="@/assets/img/logo.svg"
-            target="_blank"
-            max-width="300px"
-          ></v-img>
+      <v-container class="justify-start pa-0" style="margin-left: 2%; height: 100%">
+        <v-toolbar-title id="titleBar" v-on:click="$router.push({ name: 'Home' }).catch(err => {})">
+          <v-img alt="Logo licences nationales" src="@/assets/img/logo.svg" target="_blank" max-width="300px"></v-img>
         </v-toolbar-title>
       </v-container>
-      <v-container
-        class="d-flex justify-end mainBar ma-0 pa-0 pr-12"
-        style="max-height: 40px;"
-      >
+      <v-container class="d-flex justify-end mainBar ma-0 pa-0 pr-12" style="max-height: 40px;">
         <v-container class="pl-8 pt-2 ml-0">
-          <v-switch
-            dense
-            v-model="$vuetify.theme.dark"
-            inset
-            label="Thème sombre"
-            class="theme-selector"
-            v-on:click="saveTheme()"
-            ><template v-slot:label>
+          <v-switch dense v-model="$vuetify.theme.dark" inset label="Thème sombre" class="theme-selector"
+            v-on:click="saveTheme()"><template v-slot:label>
               <span style="color: white; font-size: 15px">Thème sombre</span>
-            </template></v-switch
-          ></v-container
-        >
-        <v-container
-          id="barreIcones"
-          class="d-flex justify-end mainBar ma-0 pa-0 pr-12"
-          style="max-height: 40px;"
-          v-if="isLoggedIn"
-        >
+            </template></v-switch>
+        </v-container>
+        <v-container id="barreIcones" class="d-flex justify-end mainBar ma-0 pa-0 pr-12" style="max-height: 40px;"
+          v-if="isLoggedIn">
           <div class="menu-slot btn-3 d-flex align-center mr-3">
             Bienvenue {{ loggedInUsername }}
           </div>
@@ -60,12 +26,7 @@
           <div v-if="isAdmin">
             <v-tooltip top max-width="20vw" open-delay="100">
               <template v-slot:activator="{ on }">
-                <v-btn
-                  @click="allerAModifierMotDePasse()"
-                  class="menu-slot btn-3"
-                  plain
-                  v-on="on"
-                >
+                <v-btn @click="allerAModifierMotDePasse()" class="menu-slot btn-3" plain v-on="on">
                   <font-awesome-icon :icon="['fas', 'lock']" size="2x" />
                 </v-btn>
               </template>
@@ -73,12 +34,7 @@
             </v-tooltip>
             <v-tooltip top max-width="20vw" open-delay="100">
               <template v-slot:activator="{ on }">
-                <v-btn
-                  @click="allerAModifierProfil()"
-                  class="menu-slot btn-3"
-                  plain
-                  v-on="on"
-                >
+                <v-btn @click="allerAModifierProfil()" class="menu-slot btn-3" plain v-on="on">
                   <font-awesome-icon :icon="['fas', 'user']" size="2x" />
                 </v-btn>
               </template>
@@ -87,13 +43,8 @@
           </div>
           <v-tooltip top max-width="20vw" open-delay="100">
             <template v-slot:activator="{ on }">
-              <v-btn
-                href="https://stp.abes.fr/node/3?origine=LicencesNationales"
-                target="_blank"
-                class="menu-slot btn-3"
-                plain
-                v-on="on"
-              >
+              <v-btn href="https://stp.abes.fr/node/3?origine=LicencesNationales" target="_blank"
+                class="menu-slot btn-3" plain v-on="on">
                 <font-awesome-icon :icon="['fas', 'comments']" size="2x" />
               </v-btn>
             </template>
@@ -102,12 +53,8 @@
           <v-divider vertical dark style="border: none;"></v-divider>
           <v-tooltip top max-width="20vw" open-delay="100">
             <template v-slot:activator="{ on }">
-              <v-btn
-                @click="seDeconnecter()"
-                class="menu-slot btn-3"
-                plain
-                v-on="on"
-              >
+              <v-btn href="https://documentation.abes.fr/aidelicencesnationales/index.html#AccederAuxLN" target="_blank"
+                class="menu-slot btn-3" plain v-on="on">
                 <font-awesome-icon :icon="['fas', 'question']" size="2x" />
               </v-btn>
             </template>
@@ -116,19 +63,14 @@
           <v-divider vertical dark style="border: none;"></v-divider>
           <v-tooltip top max-width="20vw" open-delay="100">
             <template v-slot:activator="{ on }">
-              <v-btn
-                @click="seDeconnecter()"
-                class="menu-slot btn-3"
-                plain
-                v-on="on"
-              >
+              <v-btn @click="seDeconnecter()" class="menu-slot btn-3" plain v-on="on">
                 <font-awesome-icon :icon="['fas', 'sign-out-alt']" size="2x" />
               </v-btn>
             </template>
             <span>Se déconnecter</span>
           </v-tooltip>
-        </v-container></v-container
-      >
+        </v-container>
+      </v-container>
     </v-layout>
   </v-app-bar>
 </template>
