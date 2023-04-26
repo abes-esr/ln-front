4b37b07434d4f48dcc962ca5aed4aab01c7ce3e0
Théo Chambon <chambon@abes.fr>
2023-04-26 12:43:10 +0200

    Message erreur temporaire

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,48 +1,44 @@
 <template>
-  <v-container class="d-flex justify-center">
+  <div>
+    <v-container class="d-flex justify-center">
+      <v-row align="center" justify="center">
+        <v-col xl="5" lg="7" md="9" xs="11">
+          <form-login @onChange="afficherMotDePasseOublie()" />
+
+          <div id="noAccount" class="
+                                                        mt-6
+                                                        d-flex
+                                                        justify-space-around
+                                                        flex-column flex-md-row flex-wrap
+                                                        pb-8
+                                                      ">
+            <span class="d-block full-width">
+              <h1 class="pb-2">
+                Votre établissement n'a pas encore de compte ?
+              </h1>
+            </span>
+            <v-btn class="btn-2" href="https://documentation.abes.fr/aidelicencesnationales/index.html#Beneficiaires"
+              target="_blank">Vérifier l'éligibilité
+              <font-awesome-icon :icon="['fas', 'question-circle']" class="mx-2" style="font-size: 1.1rem" />
+            </v-btn>
+            <!-- DISABLED TEMPORAIRE A SUPPRIMER ENSUITE -->
+            <v-btn class="btn-2" disabled @click="creerCompte">Créer un compte
+              <font-awesome-icon :icon="['fas', 'plus-circle']" class="mx-2" style="font-size: 1.1rem" />
+            </v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+    <!-- ALERTE TEMPORAIRE A SUPPRIMER ENSUITE -->
     <v-row align="center" justify="center">
-      <v-col xl="5" lg="7" md="9" xs="11">
-        <form-login @onChange="afficherMotDePasseOublie()" />
-
-        <div
-          id="noAccount"
-          class="
-            mt-6
-            d-flex
-            justify-space-around
-            flex-column flex-md-row flex-wrap
-            pb-8
-          "
-        >
-          <span class="d-block full-width">
-            <h1 class="pb-2">
-              Votre établissement n'a pas encore de compte ?
-            </h1>
-          </span>
-          <v-btn
-            class="btn-2"
-            href="https://documentation.abes.fr/aidelicencesnationales/index.html#Beneficiaires"
-            target="_blank"
-            >Vérifier l'éligibilité
-            <font-awesome-icon
-              :icon="['fas', 'question-circle']"
-              class="mx-2"
-              style="font-size: 1.1rem"
-            />
-          </v-btn>
-
-          <v-btn class="btn-2" @click="creerCompte"
-            >Créer un compte
-            <font-awesome-icon
-              :icon="['fas', 'plus-circle']"
-              class="mx-2"
-              style="font-size: 1.1rem"
-            />
-          </v-btn>
-        </div>
-      </v-col>
+      <v-col xl="4" lg="6" md="8" xs="10"><v-alert color="#e3040d" type="warning" border="left" dense>
+          La création de compte est indisponible pour le moment suite à un problème technique. <br />
+          Le service devrait être à nouveau disponible rapidement. <br />
+          Pour plus d'informations, contactez nous via le guichet d'assistance de l'Abes : <a
+            href="https://stp.abes.fr/node/3?origine=LicencesNationales">https://stp.abes.fr/node/3?origine=LicencesNationales</a>
+        </v-alert></v-col>
     </v-row>
-  </v-container>
+  </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
